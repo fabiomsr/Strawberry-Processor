e1c761f63aa0af6061766e0195ad2f151144b38e
fabiomsr <fabiomarcos.sr@gmail.com>
2020-03-04 21:27:46 +0100

    chore: update config files

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,10 @@
 "use strict";
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
-var del = require('del');
-var tsProject = ts.createProject("tsconfig.json");
+var tsProject = ts.createProject('tsconfig.json');
 
-gulp.task("clean:dist", function() {
- return del(['dist/**/*']);
-});
-
-gulp.task("tsc", function () {
- return tsProject.src()
- .pipe(tsProject())
- .js.pipe(gulp.dest("dist"));
-});
-
-gulp.task('default', ['clean:dist','tsc'], function () {
+gulp.task('default', function () {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest('dist'));
 });
